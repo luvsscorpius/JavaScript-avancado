1b78cbaf38d54197948154644b8983794b2261c3
and <andersonvittor1801@gmail.com>
2023-05-21 13:24:38 -0300

    Segundo commit

---
+++ 1 Let e const/index.js
@@ -0,0 +1,18 @@
+if (true) {
+    var exposta = "exposta"
+    let naoExposta = "naoExposta"
+}
+
+console.log(exposta)
+
+// quando se usa var, você pode utilizar ela fora do bloco, ja let não da
+
+// Let e const
+
+let variavel = 20 // let você consegue reatribuir a variavel
+console.log(variavel)
+variavel = 21
+console.log(variavel)
+
+// ja const voce não pode reatribuir o valor que ela foi dada primeiramente
+
