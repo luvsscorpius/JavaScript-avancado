175a235c7afa86feccec26f757b5049a73f21c98
and <andersonvittor1801@gmail.com>
2023-07-16 20:41:50 -0300

    feat(added class bola): added class bola
    Continued the aula82.js project, in this class we added the class Bola and created 3 new events.

--- Aula 82 - Atividade Prática de Classes em JavaScript/js/aula82.js
+++ Aula 82 - Atividade Prática de Classes em JavaScript/js/aula82.js
@@ -9,3 +9,36 @@ let larguraPalco = palco.offsetWidth
 let alturaPalco = palco.offsetHeight
 let bolas = []
 let numBolas = 0
+
+class Bola {
+    constructor() {
+        this.tam = Math.floor(Math.random() * 10) + 10
+        this.r = Math.floor(Math.random() * 255)
+        this.g = Math.floor(Math.random() * 255)
+        this.b = Math.floor(Math.random() * 255)
+        this.px = Math.floor(Math.random() * larguraPalco - this.tam)
+        this.py = Math.floor(Math.random() * alturaPalco - this.tam)
+    }
+}
+
+// Eventos
+
+window.addEventListener("resize", (evt) => {
+    larguraPalco = palco.offsetWidth
+    alturaPalco = palco.offsetHeight
+    console.log(larguraPalco)
+})
+
+btn_add.addEventListener("click", (evt) => {
+    const qtde = txt_qtde.value
+    for (let i = 0; i < qtde; i++) {
+        // instanciar novas bolinhas
+    }
+})
+
+btn_remover.addEventListener("click", (evt) => {
+    bolas.map((b) => {
+        // remover bolinhas
+
+    })
+})
