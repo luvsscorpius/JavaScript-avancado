d6c020bd5f14e77b4121b806370890a7b5ebfa27
and <andersonvittor1801@gmail.com>
2023-07-30 15:05:17 -0300

    feat(aula: aprendendo a trabalhar com módulos p1): aprendendo a trabalhar com módulos p1

---
+++ Aprendendo a trabalhar com módulos em JavaScript/video-aula/js/cursos.js
@@ -0,0 +1,4 @@
+const cursos = ["JavaScript", "HTML", "CSS", "Bootstrap", "Python", "PHP", "React", "Angular"]
+const carros = ["Polo", "Gol", "Gol-bolinha", "Ferrari", "Fusca"]
+
+export { cursos, carros }
---
+++ Aprendendo a trabalhar com módulos em JavaScript/video-aula/js/scripts.js
@@ -0,0 +1,7 @@
+// Importe cursos de cursos
+import { cursos } from "./cursos.js";
+import { carros } from "./cursos.js";
+
+console.log('Rodando')
+console.log(cursos)
+console.log(carros)
