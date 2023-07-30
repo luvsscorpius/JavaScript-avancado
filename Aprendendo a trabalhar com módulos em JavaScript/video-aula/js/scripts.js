a0a7d23d540ca9b23a22f975cab481556b4104e4
and <andersonvittor1801@gmail.com>
2023-07-30 15:51:13 -0300

    feat(módulos p4): utilizando módulos com classes

--- Aprendendo a trabalhar com módulos em JavaScript/video-aula/js/scripts.js
+++ Aprendendo a trabalhar com módulos em JavaScript/video-aula/js/scripts.js
@@ -16,7 +16,16 @@ import getTodosCursos from "./cursos3.js"
 // Importando tudo e dando um nome a importação
 import * as m_cursos from "./cursos3.js"
 
-// o nome da importação . te mostra todas as opções que estão disponíveis 
-console.log(m_cursos.cursos)
-console.log(m_cursos.getCurso(1))
-console.log(m_cursos.default())
+// // o nome da importação . te mostra todas as opções que estão disponíveis
+// console.log(m_cursos.cursos)
+// console.log(m_cursos.getCurso(1))
+// console.log(m_cursos.default())
+
+// P4
+
+import c from "./cursos4.js"
+
+// Chamando os métodos das classes
+c.addCurso("Vue")
+c.apagarCursos()
+console.log(c.getTodosCursos())
