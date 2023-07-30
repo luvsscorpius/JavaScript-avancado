355621900f420a1ad85bc2b11e6e798865e402a1
and <andersonvittor1801@gmail.com>
2023-07-30 15:20:37 -0300

    feat(módulos): p2 aprendendo a como utilizar módulos

--- Aprendendo a trabalhar com módulos em JavaScript/video-aula/js/scripts.js
+++ Aprendendo a trabalhar com módulos em JavaScript/video-aula/js/scripts.js
@@ -1,7 +1,14 @@
 // Importe cursos de cursos
-import { cursos } from "./cursos.js";
-import { carros } from "./cursos.js";
+// import { cursos } from "./cursos.js";
+// import { carros } from "./cursos.js";
 
-console.log('Rodando')
+// console.log('Rodando')
+// console.log(cursos)
+// console.log(carros)
+
+// Pt2
+// Exportação getTodosCursos fora da chave pois é padrão
+import getTodosCursos, { cursos } from "./cursos2.js";
+
 console.log(cursos)
-console.log(carros)
+console.log(getTodosCursos())
