b9e7532b4878fa559d593fc72b6984153d80e0f0
and <andersonvittor1801@gmail.com>
2023-07-30 15:32:33 -0300

    feat(módulos p3): aprendendo a como renomear Módulos

--- Aprendendo a trabalhar com módulos em JavaScript/video-aula/js/scripts.js
+++ Aprendendo a trabalhar com módulos em JavaScript/video-aula/js/scripts.js
@@ -8,7 +8,15 @@
 
 // Pt2
 // Exportação getTodosCursos fora da chave pois é padrão
-import getTodosCursos, { cursos } from "./cursos2.js";
+// import getTodosCursos, { cursos as c, cursos } from "./cursos2.js";
+// import { getCurso as gc } from "./cursos3.js";
 
-console.log(cursos)
-console.log(getTodosCursos())
+// Módulos renomeados
+import getTodosCursos from "./cursos3.js"
+// Importando tudo e dando um nome a importação
+import * as m_cursos from "./cursos3.js"
+
+// o nome da importação . te mostra todas as opções que estão disponíveis 
+console.log(m_cursos.cursos)
+console.log(m_cursos.getCurso(1))
+console.log(m_cursos.default())
