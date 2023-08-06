232a8bdd865dc742069fa270257bbe0a89b965c1
and <andersonvittor1801@gmail.com>
2023-08-06 14:41:16 -0300

    feat(animação): aprendendo a programar animação em JS P3
    Diferença entre setInterval e setTimeout

--- Aprendendo programar animação em Javascript/js/main.js
+++ Aprendendo programar animação em Javascript/js/main.js
@@ -13,19 +13,25 @@ let anima = null
 window.addEventListener('load', init)
 
 btnDireita.addEventListener('click', () => {
-    clearInterval(anima)
-    anima = setInterval(mover, 20, 1)
+    // clearInterval(anima)
+    // anima = setInterval(mover, 20, 1)
+    clearTimeout(anima)
+    mover(1)
 })
 
 btnEsquerda.addEventListener('click', () => {
-    clearInterval(anima)
-    anima = setInterval(mover, 20, -1)
+    // clearInterval(anima)
+    // anima = setInterval(mover, 20, -1)
+    clearTimeout(anima)
+    mover(-1)
 })
 
 btnPàrar.addEventListener('click', () => {
-    clearInterval(anima)
+    // clearInterval(anima)
+    clearTimeout(anima)
 })
 
 const mover = (dir) => {
     carro.style.left = parseInt(carro.style.left) + (10 * dir) + "px"
+    anima = setTimeout(mover, 20, dir)
 }
