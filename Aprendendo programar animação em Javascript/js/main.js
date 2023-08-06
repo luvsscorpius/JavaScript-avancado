f5e718da70581cf7141c124b841574bcb012f199
and <andersonvittor1801@gmail.com>
2023-08-06 14:25:48 -0300

    feat(animação): aprendendo a programar animação em JS P2
    Automatizando a animação

--- Aprendendo programar animação em Javascript/js/main.js
+++ Aprendendo programar animação em Javascript/js/main.js
@@ -1,21 +1,31 @@
 const carro = document.querySelector('#carro')
 const btnDireita = document.querySelector('#btnDireita')
 const btnEsquerda = document.querySelector('#btnEsquerda')
+const btnPàrar = document.querySelector('#btnPàrar')
 
 const init = () => {
     carro.style.position = 'relative'
     carro.style.left = '0px'
 }
 
+let anima = null
+
 window.addEventListener('load', init)
 
 btnDireita.addEventListener('click', () => {
-    let pos = parseInt(carro.style.left)
-    pos += 10
-    carro.style.left = `${pos}px`
-    console.log(pos)
+    clearInterval(anima)
+    anima = setInterval(mover, 20, 1)
 })
 
 btnEsquerda.addEventListener('click', () => {
-    carro.style.left = parseInt(carro.style.left) - 10 + "px"
+    clearInterval(anima)
+    anima = setInterval(mover, 20, -1)
 })
+
+btnPàrar.addEventListener('click', () => {
+    clearInterval(anima)
+})
+
+const mover = (dir) => {
+    carro.style.left = parseInt(carro.style.left) + (10 * dir) + "px"
+}
