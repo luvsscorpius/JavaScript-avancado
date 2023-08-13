96c35c16fb7987675986a7e8b4d42b5df288c02b
and <andersonvittor1801@gmail.com>
2023-08-13 13:55:05 -0300

    feat(desestruturação): aprendendo sobre desestruturação em JavaScript P2

--- Aprendendo sobre Desestruturação em Javascript/main.js
+++ Aprendendo sobre Desestruturação em Javascript/main.js
@@ -26,13 +26,47 @@
 // console.log(a)
 // console.log(b)
 
-let numeros = () => {
-    return [10, 20, 30, 40]
-}
+// let numeros = () => {
+//     return [10, 20, 30, 40]
+// }
 
-let [a, b, c, d] = numeros()
+// let [a, b, c, d] = numeros()
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+// P2
+
+let numeros = [20, 30, 40, 50, 60, 70, 80, 90]
+
+let [a, b, c, ...d] = numeros
 
 console.log(a)
 console.log(b)
 console.log(c)
 console.log(d)
+
+let obj = { nome: "and", canal: 'ande' }
+
+let { nome, canal } = obj
+console.log(nome)
+console.log(canal)
+
+const cores = () => {
+    return ["green", 'yellow', 'white', 'black']
+}
+
+let [c1, c2, , c3] = cores()
+
+console.log(c1)
+console.log(c2)
+console.log(c3)
+
+let texto = "Curso de JavaScript"
+let [t, u, i] = texto.split(" ")
+console.log(t)
+console.log(u)
+console.log(i)
+
