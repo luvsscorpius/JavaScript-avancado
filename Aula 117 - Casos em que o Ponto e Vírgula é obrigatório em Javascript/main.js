9cb22b4f62079c5477bbf8199c60c8af29208929
and <andersonvittor1801@gmail.com>
2023-08-19 16:41:20 -0300

    feat(ponto e virgula obrigatorio): casos em que o ponto e virgula é obrigatorio P1

---
+++ Aula 117 - Casos em que o Ponto e Vírgula é obrigatório em Javascript/main.js
@@ -0,0 +1,5 @@
+let i = null
+
+for (i = 0; i < 10; i++);
+// Nesse caso o ; se faz obrigatório pois não tenho um bloco de código e sim apenas uma instrução, não quero mostrar toda a sequência do for e sim somente o valor final de i
+console.log(i)
