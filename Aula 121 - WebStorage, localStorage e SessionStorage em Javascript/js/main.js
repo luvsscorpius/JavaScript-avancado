1df59f07897a0c838d0a00c956a04a25a82512f4
and <andersonvittor1801@gmail.com>
2023-08-26 16:52:19 -0300

    feat(aula 121 - webstorage): aprendendo sobre localStorage e SessionStorage em JS

---
+++ Aula 121 - WebStorage, localStorage e SessionStorage em Javascript/js/main.js
@@ -0,0 +1,19 @@
+const f_texto = document.querySelector('#f_texto')
+const p_texto = document.querySelector('#p_texto')
+const btn_texto = document.querySelector('#btn_texto')
+
+btn_texto.addEventListener('click', (e) => {
+
+})
+
+let num = 10
+
+// localStorage.setItem("Nome", "And") // localStorage.setItem adiciona um item ao localStorage
+// localStorage.setItem("Número", num)
+// p_texto.innerHTML = localStorage.getItem("Número") // Ele pega o item
+// alert(localStorage.key(1)) // Pega a chave especifica
+// localStorage.clear() // Limpa o localStorage
+// alert(localStorage.length) // Mostra o tamanho
+
+
+
