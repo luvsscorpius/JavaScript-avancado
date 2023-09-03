147f515d83c596321b87a4b736b83a8c9ce8b24e
and <andersonvittor1801@gmail.com>
2023-09-03 17:57:52 -0300

    feat(aula 135 - criando caixa de mensagem): criando uma caixa de mensagem personalizada em JS P2

--- Aula 135 - Criando caixa de mensagem personalizada em Javascript/js/msg.js
+++ Aula 135 - Criando caixa de mensagem personalizada em Javascript/js/msg.js
@@ -13,7 +13,44 @@ class Cxmsg {
     }
 
     mostrar = () => {
-        console.log(this.texto)
+        this.divmsg = document.createElement("div")
+        const style_divmsg = "display: flex;" +
+            "justify-content: center;" +
+            "align-items: center;" +
+            "position: absolute;" +
+            "top: 0px;" +
+            "left: 0px;" +
+            "width: 100%;" +
+            "height: 100vh;" +
+            "background-color: rgba(0,0,0,0.7);"
+
+        this.divmsg.setAttribute("id", "divmsg")
+        this.divmsg.setAttribute("style", style_divmsg)
+        this.destino.prepend(this.divmsg)
+
+        // Área central
+        const areaCXmsg = document.createElement("div")
+        const style_areaCxmsg = "display: flex;" +
+            "justify-content: flex-start;" +
+            "align-items: flex-start" +
+            "flex-direction: column;" +
+            "width: 300px;"
+        areaCXmsg.setAttribute("style", style_areaCxmsg)
+        this.divmsg.appendChild(areaCXmsg)
+
+        const tituloCxmsg = document.createElement("div")
+        const style_tituloCxmsg = "display: flex;" +
+            "justify-content: flex-start;" +
+            "align-items: center;" +
+            "width: 100%;" +
+            "background-color: " + this.cor + ";" +
+            "color: #fff;" +
+            "padding: 5px;" +
+            "border-radius: 5px 5px 0px 0px;"
+        tituloCxmsg.setAttribute("style", style_tituloCxmsg)
+        tituloCxmsg.innerHTML = this.titulo
+        areaCXmsg.appendChild(tituloCxmsg)
+
     }
 
     ocultar = () => {
